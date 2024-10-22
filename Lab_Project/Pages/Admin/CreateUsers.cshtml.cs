92d181b903b47cae2c5983dcc5f43cb167b87511
yousseff2255 <yousseff2255@gmail.com>
2024-10-22 22:24:00 +0300

    Added Signup ,SingIn and Index pages view icluded fontawsome

---
+++ Lab_Project/Pages/Admin/CreateUsers.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lab_Project.Pages.Admin
+{
+    public class CreateUsersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- Lab_Project/Pages/Shared/Index.cshtml.cs
+++ Lab_Project/Pages/Shared/Index.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Lab_Project.Pages
+namespace Lab_Project.Pages.Shared
 {
     public class IndexModel : PageModel
     {
